8bcbfc038b3257b77f4ece75c60db31676669a11
navjot singh <navjotsinghbe@gmail.com>
2018-07-15 16:59:01 +1000

    init commit

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+jQuery('#user-Form').on('submit', function (e) {
+    var name = $('#staffname').val()
+    var rate = $('#rate').val()
+    var data = $('#days').serializeArray()
+    obj = {};
+    $.each(data,function(){
+        obj[this.name] = this.value
+    })
+    finalObj = JSON.stringify({"name":name,"rate":rate,"timesheet":obj})
+    e.preventDefault();
+    $.ajax({
+        url: '/userData', 
+        type: 'POST', 
+        contentType: 'application/json', 
+        data: finalObj,
+        success: function(res){
+            console.log("success",res)
+        },
+        failure: function(err){
+            console.log("error",err)
+        }
+    })
+
+});
